4487880ea1a9bd712ccd8d9785e7e9c15448d3a6
obendekellyofuje <kellyobende@gmail.com>
2022-10-20 19:24:24 +0100

    ff

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=cidr
+export POSTGRES_PASSWORD=kelloggs964554
+export POSTGRES_HOST=cidr.cela7ika3yxh.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=test-129137567697-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
